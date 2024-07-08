58bb0eb384b1451b0789dcadb544116af5cc74cd
anubhavt537 <anubhavt537@gmail.com>
2024-07-08 21:32:13 +0530

    connection mongodb

---
+++ src/index.js
@@ -0,0 +1,47 @@
+
+import dotenv from 'dotenv'
+// import mongoose from 'mongoose';
+//   import {DB_NAME} from './constants'
+//   import express from 'express'
+  import connectDB from './db/index.js';
+
+  dotenv.config({
+    path:'./env'
+  })
+   
+  connectDB()
+  
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  /*
+  APPROACH 1
+  const app=express()
+( async()=>{
+    try {
+       await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.error("ERROR IN EXPRESS" ,error)
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`app is listening on port ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.error("ERROR",error)
+        throw error;
+    }
+})()
+*/
